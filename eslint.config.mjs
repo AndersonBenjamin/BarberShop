c7ca6d2ba091b32e104adace87721288feb42c75
Anderson Targino <anderson.targino@dnkinfotelecom.com.br>
2025-02-05 12:54:56 -0300

    remove rules eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Desabilitar a regra de 'any'
+      // Adicione outras regras que você precise ajustar
+    }
+  }
 ];
 
 export default eslintConfig;
