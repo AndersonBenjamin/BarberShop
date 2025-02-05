9ed53edad8edfd49bd8c74899899825016fb884f
Anderson Targino <anderson.targino@dnkinfotelecom.com.br>
2025-02-05 17:48:08 -0300

    style: ensure footer is always on the bottom

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} dark`}>
         <AuthProvider>
-          {children}
+          <div className="flex-1">{children}</div>
           <Toaster></Toaster>
           <Footer></Footer>
         </AuthProvider>
