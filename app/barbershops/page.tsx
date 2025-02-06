34ab02e5adac9636b83d9af00a6aeed3e91fbb32
Anderson Targino <anderson.targino@dnkinfotelecom.com.br>
2025-02-06 18:22:49 -0300

    fix search render

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -4,47 +4,51 @@ import Header from "../_components/header";
 import { db } from "../_lib/prisma";
 import Search from "../(home)/_components/search";
 
-interface BarberShopsPageProps {
-    searchParams: {
-        search?: string | undefined;
-    };
+interface BarbershopsPageProps {
+  searchParams: {
+    search?: string;
+  };
 }
 
-const BarberShopsPage = async ({ searchParams }: BarberShopsPageProps) => {
-    if (!searchParams.search) {
-        redirect('/')
-    }
-    const barbershops = await db.barbershop.findMany({
-        where: {
-            name: {
-                contains: searchParams.search,
-                mode: 'insensitive'
-            },
-        },
-    });
-    return (
-        <>
-            <Header />
 
-            <div className="px-5 py-6 flex flex-col gap-6">
-                <Search defaultValues={{
-                    search: searchParams.search,
-                }} />
-                <h1 className="text-gray-400 font-bold text-xs uppercase">Resultado para &quot;{searchParams.search}&quot;</h1>
+const BarbershopsPage = async ({ searchParams }: { searchParams: Promise<BarbershopsPageProps['searchParams']> }) => {
+  const { search } = await searchParams; // Aguarda a resolução de `params`
+  console.log('Chegou em barbershops')
+  if (!search) {
+    return redirect("/");
+  }
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: search,
+        mode: "insensitive",
+      },
+    },
+  });
 
-                <div className="grid grid-cols-2 mt-3 gap-4">
-                    {barbershops.map((barbershop) => (
+  return (
+    <>
+      <Header />
 
-                        <div key={barbershop.id} className="w-full">
-                            <BarbershopItem barbershop={barbershop} />
-                        </div>
-                    ))}
-                </div>
+      <div className="px-5 py-6 flex flex-col gap-6">
+        <Search
+          defaultValues={{
+            search: search,
+          }}
+        />
+
+        <h1 className="text-gray-400 font-bold text-xs uppercase">Resultados para &quot;{search}&quot;</h1>
+
+        <div className="grid grid-cols-2 gap-4">
+          {barbershops.map((barbershop) => (
+            <div key={barbershop.id} className="w-full">
+              <BarbershopItem barbershop={barbershop} />
             </div>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-}
-
-
-export default BarberShopsPage;
+export default BarbershopsPage;
